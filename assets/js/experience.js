19fe644a9ed0d1a58658f074a3cb063622a8324f
Shuvojit Das <Shuvojitdas2000@gmail.com>
2023-03-19 16:10:29 +0530

    changes

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -16,15 +16,15 @@ const exp = [
     title: "Junior Developer",
     cardImage: "assets/images/experience-page/zcs.png",
     place: "Zenais Consultancy Services Pvt Ltd",
-    time: "(Sep,2021 - Feb,2022)",
+    time: "(Sep,2021 - Jan,2022)",
     desp: "<li>Worked as a Junior Full Stack Developer</li> <li>Increased functionality,efficiency,UI and improving the overall workflow</li>",
   },
   {
-    title: "Project Engineer",
-    cardImage: "assets/images/experience-page/Wipro.png",
-    place: " Wipro Limited",
-    time: "(March,2022 - Current)",
-    desp: "<li>Working as a Intern.</li><li>Current Domain is JAVA J2EE </li>"
+    title: "Associate Technical Developer",
+    cardImage: "assets/images/experience-page/Trimplin.jpeg",
+    place: " Trimplin Group",
+    time: "(Jan,2022 - Current)",
+    desp: "<li>Working as a Technical Developer in the domain of Artificial Intelligence, Machine Learning, Data Analytics & Big Data.</li>"
   },
 ];
 
